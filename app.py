43de609087a8128ffb4951f6b8833495d0391074
IllonaSab <sabouillona@gmail.com>
2025-03-04 12:31:15 +0100

    hello

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ import cv2
 
 
 # TensorFlow and tf.keras
-from tensorflow.keras.applications.imagenet_utils import
-preprocess_input, decode_predictions
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import
-MobileNetV2
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.preprocessing import image 
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 
 
 # Modèle ImageNet
